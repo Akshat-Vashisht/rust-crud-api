6172db0a0085b7ffc8b0b29f54fda7ccd95e24c6
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-12-26 11:43:06 +0530

    added middle-ware

--- src/api/items/item_route.rs
+++ src/api/items/item_route.rs
@@ -1,19 +1,20 @@
-use crate::{api::items::item_handler, AppState};
+use crate::{api::items::item_handler, jwtauth::auth, AppState};
 
-use axum::{routing::get, Router};
+use axum::{middleware, routing::get, Router};
 
 pub fn item_routes(app_state: AppState) -> Router {
     Router::new()
         .route(
             "/",
-            get(item_handler::get_all_items)
-                .post(item_handler::create_item)
-            
+            get(item_handler::get_all_items).post(item_handler::create_item),
         )
+        .route_layer(middleware::from_fn_with_state(app_state.clone(), auth))
         .route(
             "/:id",
             get(item_handler::get_item_by_id)
                 .put(item_handler::update_item)
-                .delete(item_handler::delete_item)
-        ).with_state(app_state.clone())
+                .delete(item_handler::delete_item),
+        )
+        .route_layer(middleware::from_fn_with_state(app_state.clone(), auth))
+        .with_state(app_state.clone())
 }
